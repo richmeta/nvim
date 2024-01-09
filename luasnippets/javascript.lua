d5944b19a96734b78631c0932610a08fa8df9138
Richard French <richard.french.uk@googlemail.com>
2024-01-09 08:47:01 +0000

    js snippets

--- luasnippets/javascript.lua
+++ luasnippets/javascript.lua
@@ -1,16 +1,25 @@
 
 local ls = require("luasnip")
 local fmt = require("luasnip.extras.fmt").fmt
-local rep = require("luasnip.extras").rep
 local s = ls.snippet
-local f = ls.function_node
 local i = ls.insert_node
 local t = ls.text_node
-local u = require("user.snip")
+-- local u = require("user.snip")
+-- local rep = require("luasnip.extras").rep
+-- local f = ls.function_node
 
 return {
     -- 
     s( { trig = "#!", dscr = "shebang", snippetType = "autosnippet" },
         t{'#!/usr/bin/env node', ''}
     ),
+
+    s( { trig = "cl", dscr = "console log" },
+        fmt('console.log({});', i(0))
+    ),
+
+    s( { trig = "ce", dscr = "console error" },
+        fmt('console.error({});', i(0))
+    ),
+
 }
